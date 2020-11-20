760c1a5c26942d773ec412743744f6190346e26e
Chaojun <suncj2@lenovo.com>
2020-11-20 10:27:43 +0800

    Reader to kotlin

---
+++ src/main/kotlin/com/hagoapp/f2t/datafile/Reader.kt
@@ -0,0 +1,14 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.hagoapp.f2t.datafile
+
+import java.io.Closeable
+
+interface Reader : Closeable, MutableIterator<DataRow> {
+    fun open(fileInfo: FileInfo?)
+    fun findColumns(): List<ColumnDefinition>
+    fun inferColumnTypes(sampleRowCount: Long = -1): List<ColumnDefinition>
+}
