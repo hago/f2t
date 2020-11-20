d09a98a67038b57d62dda56568379145bc64b20d
Chaojun <suncj2@lenovo.com>
2020-11-20 18:25:36 +0800

    fix

--- src/main/kotlin/com/hagoapp/f2t/datafile/FileInfoCsv.kt
+++ src/main/kotlin/com/hagoapp/f2t/datafile/FileInfoCsv.kt
@@ -6,10 +6,19 @@
 
 package com.hagoapp.f2t.datafile
 
+import com.hagoapp.f2t.F2TException
+import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets
 
 class FileInfoCsv(filename: String) : FileInfo(filename) {
-    var encoding = StandardCharsets.UTF_8.displayName()
+    var encoding: String = StandardCharsets.UTF_8.displayName()
+        set(value) {
+            try {
+                Charset.forName(value)
+            } catch (e: Throwable) {
+                throw F2TException("$value is not valid charset", e)
+            }
+        }
     var quote = '"'
-    var delimiter = ","
+    var delimiter = ','
 }
