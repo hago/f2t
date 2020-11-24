e1b28fe9891041f14e937565b7f73c605ba2e996
Chaojun <suncj2@lenovo.com>
2020-11-24 11:17:32 +0800

    add db types and config

---
+++ src/main/java/com/hagoapp/f2t/database/config/DbConfig.java
@@ -0,0 +1,61 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.f2t.database.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.hagoapp.f2t.database.DbType;
+
+public class DbConfig {
+    protected DbType dbType;
+    protected String username;
+    protected String password;
+    protected String databaseName;
+
+    public DbType getDbType() {
+        return dbType;
+    }
+
+    public void setDbType(DbType dbType) {
+        this.dbType = dbType;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public String toString() {
+        Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+        return gson.toJson(this);
+    }
+}
---
+++ src/main/java/com/hagoapp/f2t/database/config/PgSqlConfig.java
@@ -0,0 +1,28 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.f2t.database.config;
+
+public class PgSqlConfig extends DbConfig {
+    private String host;
+    private int port = 5432;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
