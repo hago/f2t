8d66e05994f90d5912804732b754ffcaad584449
Chaojun <suncj2@lenovo.com>
2020-11-26 18:32:17 +0800

    open connection before return it

---
+++ src/main/kotlin/com/hagoapp/f2t/database/DbConnectionFactory.kt
@@ -0,0 +1,24 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.f2t.database
+
+import com.hagoapp.f2t.F2TException
+import com.hagoapp.f2t.database.config.DbConfig
+
+class DbConnectionFactory {
+    companion object {
+        @JvmStatic
+        fun createDbConnection(dbConfig: DbConfig): DbConnection {
+            val connection = when(dbConfig.dbType) {
+                DbType.PostgreSql -> PgSqlConnection()
+                else -> throw F2TException("Unknown database type: ${dbConfig.dbType.name}")
+            }
+            connection.open(dbConfig)
+            return connection
+        }
+    }
+}
