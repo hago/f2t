59236f949e80b4cc7ce857b08939608048a8f6eb
Hago <suncjs@hagoapp.com>
2020-12-22 17:08:59 +0800

    add process; not done

---
+++ src/main/kotlin/com/hagoapp/f2t/D2TProcess.kt
@@ -0,0 +1,113 @@
+package com.hagoapp.f2t
+
+import com.hagoapp.f2t.database.DbConnection
+import com.hagoapp.f2t.database.DbConnectionFactory
+import com.hagoapp.f2t.database.TableName
+import com.hagoapp.f2t.database.config.DbConfig
+import com.hagoapp.f2t.datafile.ParseResult
+import java.lang.reflect.Method
+import java.time.Instant
+
+class D2TProcess(dTable: DataTable, dbConfig: DbConfig, f2TConfig: F2TConfig) {
+    private var dataTable: DataTable = dTable
+    private val connection: DbConnection
+    private var config: F2TConfig = f2TConfig
+    private val logger = F2TLogger.getLogger()
+    private var tableMatchedFile = false
+    private val table: TableName
+    private var batchNum = -1L
+
+    companion object {
+        private val methods = mutableMapOf<String, Method>()
+
+        init {
+            for (method in ParseObserver::class.java.declaredMethods) {
+                methods[method.name] = method
+            }
+        }
+    }
+
+    init {
+        if (config.isAddBatch && (config.batchColumnName == null)) {
+            logger.error("identity column can't be null when addIdentity set to true")
+            throw F2TException("identity column can't be null when addIdentity set to true")
+        }
+        table = TableName(config.targetTable, config.targetSchema ?: "")
+        connection = DbConnectionFactory.createDbConnection(dbConfig)
+    }
+
+    fun run(): ParseResult {
+        val parseResult = ParseResult()
+        try {
+            if (!prepareTable()) {
+                throw Exception("DataTable object doesn't match existing table $table in database, or new table creation is forbidden.")
+            }
+            batchNum = Instant.now().toEpochMilli()
+            dataTable.rows.forEachIndexed { i, row ->
+                try {
+                    onRowRead(row)
+                } catch (e: Throwable) {
+                    parseResult.addError(i.toLong(), e)
+                }
+            }
+        } catch (e: Throwable) {
+            parseResult.addError(-1L, e)
+        } finally {
+            parseResult.end()
+            try {
+                connection.close()
+            } catch (e: Throwable) {
+                logger.error("error: $e in connection close")
+            }
+        }
+        return parseResult
+    }
+
+    private fun prepareTable(): Boolean {
+        if (connection.isTableExists(table)) {
+            val tblDef = connection.getExistingTableDefinition(table)
+            val difference = tblDef.diff(dataTable.columnDefinition.toSet(), connection.isCaseSensitive())
+            return if (!difference.noDifference) {
+                logger.error("table $table existed and differ from data to be imported, all follow-up database actions aborted")
+                logger.error(difference.toString())
+                false
+            } else {
+                if (config.isClearTable) {
+                    connection.clearTable(table)
+                    logger.warn("table ${connection.getFullTableName(table)} cleared")
+                }
+                connection.prepareInsertion(table, tblDef)
+                logger.info("table $table found and matches")
+                true
+            }
+        } else {
+            if (config.isCreateTableIfNeeded) {
+                val tblDef = TableDefinition(dataTable.columnDefinition.toSet())
+                return try {
+                    connection.createTable(table, tblDef)
+                    connection.prepareInsertion(table, tblDef)
+                    tableMatchedFile = true
+                    logger.info("table $table created")
+                    true
+                } catch (e: Throwable) {
+                    logger.error("Error occurs when creating table $table: ${e.message}")
+                    false
+                }
+            } else {
+                logger.error("table $table not existed and auto creation is not enabled, all follow-up database actions aborted")
+                return false
+            }
+        }
+    }
+
+    private fun onRowRead(row: DataRow) {
+        if (tableMatchedFile) {
+            val r = if (batchNum < 0) row else DataRow(
+                row.rowNo, row.cells.toMutableList()
+                    .plus(DataCell(batchNum, row.cells.size))
+            )
+            connection.writeRow(table, r)
+        }
+    }
+
+}
