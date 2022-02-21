c3e5b52740c1522f9b09e172b378e68498b0db33
Chaojun Sun <suncj2@lenovo.com>
2022-02-21 11:23:40 +0800

    fix ascii detect

--- src/main/kotlin/com/hagoapp/util/NumericUtils.kt
+++ src/main/kotlin/com/hagoapp/util/NumericUtils.kt
@@ -7,18 +7,20 @@
 package com.hagoapp.util
 
 class NumericUtils {
-    fun detectPrecision(input: Any): Pair<Int, Int> {
-        val s = input.toString()
-        return try {
-            s.toDouble()
-            val ss = s.split("\\.").toTypedArray()
-            if (ss.size != 2) {
+    companion object {
+        fun detectPrecision(input: Any): Pair<Int, Int> {
+            val s = input.toString()
+            return try {
+                s.toDouble()
+                val ss = s.split("\\.").toTypedArray()
+                if (ss.size != 2) {
+                    Pair(0, 0)
+                } else {
+                    Pair(ss[0].length, ss[1].length)
+                }
+            } catch (ignored: NumberFormatException) {
                 Pair(0, 0)
-            } else {
-                Pair(ss[0].length, ss[1].length)
             }
-        } catch (ignored: NumberFormatException) {
-            Pair(0, 0)
         }
     }
 }
