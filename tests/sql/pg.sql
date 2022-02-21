bbe4d3bdef720ffa3e518fd3015f87b5f778ad5b
Chaojun Sun <suncjs@lenovo.com>
2022-02-22 00:26:12 +0800

    add table to test pg def

---
+++ tests/sql/pg.sql
@@ -0,0 +1,17 @@
+create table if not exists demo (
+    id serial not null,
+    "name" varchar(100),
+    "description" text,
+    age smallint,
+    "number" int,
+    long bigint,
+    floatnumber float,
+    doublenumber double precision,
+    decimaldefault decimal,
+    decimalnumber decimal(20,5),
+    tswz timestamp with time zone,
+    tsoz timestamp without time zone,
+    primary key(id, "name"),
+    unique("name", age),
+    unique("number", age, long)
+);
