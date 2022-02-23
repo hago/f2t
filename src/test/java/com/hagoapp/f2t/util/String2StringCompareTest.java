7007b79e85c7a4bbf02bc72fa12bfeb5afd3deb0
Chaojun Sun <suncj2@lenovo.com>
2022-02-23 15:41:58 +0800

    complete string to string column compare test

--- src/test/java/com/hagoapp/f2t/util/String2StringCompareTest.java
+++ src/test/java/com/hagoapp/f2t/util/String2StringCompareTest.java
@@ -9,7 +9,9 @@ package com.hagoapp.f2t.util;
 import com.hagoapp.f2t.ColumnDefinition;
 import com.hagoapp.f2t.FileColumnDefinition;
 import com.hagoapp.f2t.Quartet;
+import com.hagoapp.f2t.Quintet;
 import com.hagoapp.f2t.compare.ColumnComparator;
+import kotlin.Pair;
 import kotlin.Triple;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -77,6 +79,28 @@ public class String2StringCompareTest {
             dc.getTypeModifier().setMaxLength(i.getThird());
             var r = ColumnComparator.Companion.compare(fc, dc);
             Assertions.assertEquals(r.isTypeMatched() && r.getCanLoadDataFrom(), i.getFourth());
+        }
+    }
+
+    private final List<Quintet<JDBCType, Integer, JDBCType, Integer, Pair<Boolean, Boolean>>> stringStringCases = List.of(
+            new Quintet<>(CHAR, 20, CHAR, 1000000, new Pair<>(true, true)),
+            new Quintet<>(CHAR, Integer.MAX_VALUE, VARCHAR, 99999999, new Pair<>(true, false)),
+            new Quintet<>(VARCHAR, Integer.MAX_VALUE, CHAR, 798798739, new Pair<>(true, false)),
+            new Quintet<>(VARCHAR, 335, VARCHAR, Integer.MAX_VALUE, new Pair<>(true, true))
+    );
+
+    @Test
+    public void string2StringColumnTest() {
+        for (var i : stringStringCases) {
+            var fc = new FileColumnDefinition();
+            fc.setDataType(i.getFirst());
+            fc.getTypeModifier().setMaxLength(i.getSecond());
+            var dc = new ColumnDefinition();
+            dc.setDataType(i.getThird());
+            dc.getTypeModifier().setMaxLength(i.getFourth());
+            var r = ColumnComparator.Companion.compare(fc, dc);
+            Assertions.assertEquals(i.getFifth().getFirst(), r.isTypeMatched());
+            Assertions.assertEquals(i.getFifth().getSecond(), r.getCanLoadDataFrom());
         }
     }
 }
