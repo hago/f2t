4193234850c743b8e96255988efc7ecd9df274c9
Chaojun Sun <suncj2@lenovo.com>
2022-02-24 16:57:32 +0800

    add column comparator for int to int

---
+++ src/main/kotlin/com/hagoapp/f2t/compare/column/Int2IntComparator.kt
@@ -0,0 +1,43 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.f2t.compare.column
+
+import com.hagoapp.f2t.ColumnDefinition
+import com.hagoapp.f2t.FileColumnDefinition
+import com.hagoapp.f2t.compare.ColumnComparator
+import com.hagoapp.f2t.compare.CompareColumnResult
+import java.sql.JDBCType
+import java.sql.JDBCType.*
+
+class Int2IntComparator : ColumnComparator.Comparator {
+
+    companion object {
+        val ranks = mapOf(
+            TINYINT to 0,
+            SMALLINT to 1,
+            INTEGER to 2,
+            BIGINT to 3
+        )
+    }
+
+    override fun dataCanLoadFrom(
+        fileColumnDefinition: FileColumnDefinition,
+        dbColumnDefinition: ColumnDefinition
+    ): CompareColumnResult {
+        val r0 = ranks.getValue(fileColumnDefinition.dataType)
+        val r1 = ranks.getValue(dbColumnDefinition.dataType)
+        return CompareColumnResult(isTypeMatched = r0 == r1, r0 <= r1)
+    }
+
+    override fun supportSourceTypes(): Set<JDBCType> {
+        return setOf(TINYINT, SMALLINT, INTEGER, BIGINT)
+    }
+
+    override fun supportDestinationTypes(): Set<JDBCType> {
+        return setOf(TINYINT, SMALLINT, INTEGER, BIGINT)
+    }
+}
