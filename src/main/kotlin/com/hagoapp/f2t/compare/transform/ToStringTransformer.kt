2272de622d8af80c852d7254e5beaef9dab986bb
Chaojun Sun <suncj2@lenovo.com>
2022-03-07 18:14:02 +0800

    draft float2float and all to string transformers

---
+++ src/main/kotlin/com/hagoapp/f2t/compare/transform/ToStringTransformer.kt
@@ -0,0 +1,62 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.f2t.compare.transform
+
+import com.hagoapp.f2t.ColumnDefinition
+import com.hagoapp.f2t.FileColumnDefinition
+import com.hagoapp.f2t.compare.ColumnComparator
+import com.hagoapp.f2t.util.DateTimeTypeUtils
+import java.sql.JDBCType
+import java.sql.JDBCType.*
+import java.time.temporal.Temporal
+
+class ToStringTransformer : ColumnComparator.Transformer {
+    override fun transform(
+        src: Any?,
+        fileColumnDefinition: FileColumnDefinition,
+        dbColumnDefinition: ColumnDefinition,
+        vararg extra: String
+    ): Any? {
+        return when (dbColumnDefinition.dataType) {
+            DATE -> {
+                src as Temporal? ?: return null
+                val fmt = if (extra.isNotEmpty()) extra[0] else null
+                DateTimeTypeUtils.getDateFormatter(fmt).format(src)
+            }
+            TIME -> {
+                src as Temporal? ?: return null
+                val fmt = if (extra.isNotEmpty()) extra[0] else null
+                DateTimeTypeUtils.getDTimeFormatter(fmt).format(src)
+            }
+            TIMESTAMP -> {
+                src as Temporal? ?: return null
+                val fmt = if (extra.isNotEmpty()) extra[0] else null
+                DateTimeTypeUtils.getDateTimeFormatter(fmt).format(src)
+            }
+            TIMESTAMP_WITH_TIMEZONE -> {
+                src as Temporal? ?: return null
+                val fmt = if (extra.isNotEmpty()) extra[0] else null
+                DateTimeTypeUtils.getDateTimeFormatter(fmt).format(src)
+            }
+            else -> src?.toString()
+        }
+    }
+
+    override fun supportSourceTypes(): Set<JDBCType> {
+        return setOf(
+            BOOLEAN,
+            CHAR, VARCHAR, CLOB, NCHAR, NVARCHAR, NCLOB,
+            SMALLINT, TINYINT, INTEGER, BIGINT,
+            FLOAT, DOUBLE, DECIMAL,
+            DATE, TIME, TIMESTAMP, TIMESTAMP_WITH_TIMEZONE
+        )
+    }
+
+    override fun supportDestinationTypes(): Set<JDBCType> {
+        return setOf(CHAR, VARCHAR, CLOB, NCHAR, NVARCHAR, NCLOB)
+    }
+}
