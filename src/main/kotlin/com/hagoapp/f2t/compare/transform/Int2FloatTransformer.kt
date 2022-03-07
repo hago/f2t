70473f15223c7be728c2b1f533c3bce85fc53bb2
Chaojun Sun <suncj2@lenovo.com>
2022-03-07 16:16:37 +0800

    draft int-* transformers

---
+++ src/main/kotlin/com/hagoapp/f2t/compare/transform/Int2FloatTransformer.kt
@@ -0,0 +1,43 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.f2t.compare.transform
+
+import com.hagoapp.f2t.ColumnDefinition
+import com.hagoapp.f2t.FileColumnDefinition
+import com.hagoapp.f2t.compare.ColumnComparator
+import java.math.BigDecimal
+import java.sql.JDBCType
+import java.sql.JDBCType.*
+
+class Int2FloatTransformer : ColumnComparator.Transformer {
+    override fun transform(
+        src: Any?,
+        fileColumnDefinition: FileColumnDefinition,
+        dbColumnDefinition: ColumnDefinition,
+        vararg extra: String
+    ): Any? {
+        if ((src !is Float?) && (src !is Double?) && (src !is BigDecimal?)) {
+            throw Exception("Not integer input to transform to float")
+        }
+        src as Number?
+        return if (src == null) null
+        else when (dbColumnDefinition.dataType) {
+            FLOAT -> src.toFloat()
+            DOUBLE -> src.toDouble()
+            DECIMAL -> BigDecimal.valueOf(src.toDouble())
+            else -> null
+        }
+    }
+
+    override fun supportSourceTypes(): Set<JDBCType> {
+        return setOf(TINYINT, SMALLINT, INTEGER, BIGINT)
+    }
+
+    override fun supportDestinationTypes(): Set<JDBCType> {
+        return setOf(FLOAT, DOUBLE, DECIMAL)
+    }
+}
---
+++ src/main/kotlin/com/hagoapp/f2t/compare/transform/Int2IntTransformer.kt
@@ -0,0 +1,43 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.f2t.compare.transform
+
+import com.hagoapp.f2t.ColumnDefinition
+import com.hagoapp.f2t.FileColumnDefinition
+import com.hagoapp.f2t.compare.ColumnComparator
+import java.sql.JDBCType
+import java.sql.JDBCType.*
+
+class Int2IntTransformer : ColumnComparator.Transformer {
+    override fun transform(
+        src: Any?,
+        fileColumnDefinition: FileColumnDefinition,
+        dbColumnDefinition: ColumnDefinition,
+        vararg extra: String
+    ): Any? {
+        if ((src !is Byte?) && (src !is Short?) && (src !is Int?) && (src !is Long?)) {
+            throw Exception("Not integer input to transform to integer")
+        }
+        src as Number?
+        return if (src == null) null
+        else when (dbColumnDefinition.dataType) {
+            TINYINT -> src.toByte()
+            SMALLINT -> src.toShort()
+            INTEGER -> src.toInt()
+            BIGINT -> src.toLong()
+            else -> null
+        }
+    }
+
+    override fun supportSourceTypes(): Set<JDBCType> {
+        return setOf(TINYINT, SMALLINT, INTEGER, BIGINT)
+    }
+
+    override fun supportDestinationTypes(): Set<JDBCType> {
+        return setOf(TINYINT, SMALLINT, INTEGER, BIGINT)
+    }
+}
