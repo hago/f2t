3b24cb15f65352182784bd000ba36abdabaecb9d
Chaojun Sun <suncjs@hagoapp.com>
2022-12-01 18:14:03 +0800

    add column definition for parquet

---
+++ src/main/java/com/hagoapp/f2t/datafile/parquet/ParquetColumnDefinition.java
@@ -0,0 +1,28 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.f2t.datafile.parquet;
+
+import com.hagoapp.f2t.ColumnDefinition;
+import org.apache.parquet.schema.PrimitiveType;
+
+/**
+ * Definition for a column from parquet.
+ *
+ * @author suncjs
+ * @since 0.7.5
+ */
+public class ParquetColumnDefinition extends ColumnDefinition {
+    private PrimitiveType parquetType;
+
+    public PrimitiveType getParquetType() {
+        return parquetType;
+    }
+
+    public void setParquetType(PrimitiveType parquetType) {
+        this.parquetType = parquetType;
+    }
+}
