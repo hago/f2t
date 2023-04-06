0bd1dbe4dfbf16c4c09506d3875d783c3668dba7
Chaojun Sun <suncjs@outlook.com>
2023-04-06 18:07:56 +0800

    add pg's partition demo table in sample

--- tests/sql/pg.sql
+++ tests/sql/pg.sql
@@ -18,3 +18,14 @@ create table if not exists demo (
     unique("name", age),
     unique("number", age, long)
 );
+
+create table if not exists partdemo (
+    id int not null,
+    primary key(id)
+) PARTITION BY RANGE (id);
+
+create table if not exists partdemo_below_100 partition of partdemo
+for values from (MINVALUE) to (100);
+create table if not exists partdemo_100_and_above partition of partdemo
+for values from (100) to (MAXVALUE);
+
