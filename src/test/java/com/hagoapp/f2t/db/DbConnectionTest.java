ac953408fd633f9fbfce08a1618279716f0e3804
Chaojun Sun <suncjs@hagoapp.com>
2023-12-18 16:50:45 +0800

    pass test for more coverage

--- src/test/java/com/hagoapp/f2t/db/DbConnectionTest.java
+++ src/test/java/com/hagoapp/f2t/db/DbConnectionTest.java
@@ -6,6 +6,10 @@
 
 package com.hagoapp.f2t.db;
 
+import com.hagoapp.f2t.ColumnDefinition;
+import com.hagoapp.f2t.F2TException;
+import com.hagoapp.f2t.TableDefinition;
+import com.hagoapp.f2t.TableUniqueDefinition;
 import com.hagoapp.f2t.database.DbConnectionFactory;
 import com.hagoapp.f2t.database.TableName;
 import com.hagoapp.f2t.database.config.DbConfigReader;
@@ -15,6 +19,8 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.JDBCType;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -33,6 +39,12 @@ class DbConnectionTest {
 
     private static final String SKIP_TEST_DB_TYPE = "f2t.dbtests.skip";
     private static final List<String> skipped = new ArrayList<>();
+    private static final List<ColumnDefinition> testColumnsDefinition = List.of(
+            new ColumnDefinition("intCol", JDBCType.INTEGER),
+            new ColumnDefinition("intString", JDBCType.CLOB),
+            new ColumnDefinition("intBool", JDBCType.BOOLEAN),
+            new ColumnDefinition("intTimestamp", JDBCType.TIMESTAMP)
+    );
 
     @BeforeAll
     static void init() {
@@ -74,6 +86,47 @@ class DbConnectionTest {
                     }
                 }
             });
+        }
+    }
+
+    private final String testTable = "test_tbl";
+
+    @Test
+    void testWriteOperations() throws F2TException, SQLException {
+        for (var configFile : testConfigFiles) {
+            if (skipped.contains(configFile)) {
+                logger.debug("skip {}", configFile);
+                continue;
+            } else {
+                logger.debug("testing using {}", configFile);
+            }
+            var config = DbConfigReader.readConfig(configFile);
+            try (var conn = config.createConnection()) {
+                try (var con = DbConnectionFactory.createDbConnection(conn)) {
+                    var table = new TableName(testTable, con.getDefaultSchema());
+                    // drop test table in case left by last failed tests.
+                    con.dropTable(table);
+                    var caseSensitive = con.isCaseSensitive();
+                    var primaryKeyDef = new TableUniqueDefinition<>(
+                            "pkey_unit_test",
+                            testColumnsDefinition.subList(0, 1),
+                            caseSensitive
+                    );
+                    var def = new TableDefinition<>(
+                            testColumnsDefinition,
+                            caseSensitive,
+                            primaryKeyDef,
+                            false
+                    );
+                    con.createTable(table, def);
+                    con.clearTable(table);
+                    Assertions.assertTrue(con.isTableExists(table));
+                    var r = con.dropTable(table);
+                    logger.debug("drop message: {}", r.getSecond());
+                    Assertions.assertTrue(r.getFirst());
+                    Assertions.assertFalse(con.isTableExists(table));
+                }
+            }
         }
     }
 }
