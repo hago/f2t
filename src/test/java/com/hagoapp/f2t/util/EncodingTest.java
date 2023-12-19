cd39f5dc897bd118a73d0853c54b35a74eb7f96a
Chaojun Sun <suncjs@hagoapp.com>
2023-12-19 13:32:23 +0800

    more tests

--- src/test/java/com/hagoapp/f2t/util/EncodingTest.java
+++ src/test/java/com/hagoapp/f2t/util/EncodingTest.java
@@ -10,6 +10,11 @@ import com.hagoapp.util.EncodingUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 class EncodingTest {
@@ -32,5 +37,17 @@ class EncodingTest {
         Assertions.assertEquals(len, s.length());
         s = EncodingUtils.createRandomString(3, "x");
         Assertions.assertEquals("xxx", s);
+    }
+
+    @Test
+    void testGuessEncoding() throws IOException {
+        var expect = StandardCharsets.UTF_8.displayName();
+        var file = "tests/csv/shuihudata.csv";
+        Assertions.assertEquals(expect, EncodingUtils.guessEncoding(file));
+        Assertions.assertEquals(expect, EncodingUtils.guessEncoding(new File(file)));
+        try (var fis = new FileInputStream(file)) {
+            var enc = EncodingUtils.guessEncoding(fis);
+            Assertions.assertEquals(expect, enc);
+        }
     }
 }
