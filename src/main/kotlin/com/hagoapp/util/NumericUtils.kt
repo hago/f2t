f03fbee8c7842759a6134b86e73b9672314a1134
Chaojun Sun <suncjs@hagoapp.com>
2023-12-25 18:03:54 +0800

    fix NumericUtils

--- src/main/kotlin/com/hagoapp/util/NumericUtils.kt
+++ src/main/kotlin/com/hagoapp/util/NumericUtils.kt
@@ -22,6 +22,7 @@ class NumericUtils {
          * @param input the source input
          * @return  a pair of integer, the first one is the count of integral part and the second is for fraction part
          */
+        @JvmStatic
         fun detectPrecision(input: Any): Pair<Int, Int> {
             val s = input.toString()
             return try {
@@ -43,6 +44,7 @@ class NumericUtils {
          * @param input numeric value in BigDecimal type
          * @return true if within valid ranges of double type, otherwise false
          */
+        @JvmStatic
         fun isDecimalInDoubleRange(input: BigDecimal): Boolean {
             return input < Double.MAX_VALUE.toBigDecimal() &&
                     input > Double.MIN_VALUE.toBigDecimal()
@@ -54,6 +56,7 @@ class NumericUtils {
          * @param input numeric value in BigDecimal type
          * @return true if within valid ranges of float type, otherwise false
          */
+        @JvmStatic
         fun isDecimalInFloatRange(input: BigDecimal): Boolean {
             return input < Float.MAX_VALUE.toBigDecimal() &&
                     input > Float.MIN_VALUE.toBigDecimal()
@@ -65,18 +68,20 @@ class NumericUtils {
          * @param input numeric value in BigDecimal type
          * @return true if within valid ranges of Int type, otherwise false
          */
+        @JvmStatic
         fun isDecimalIntegralValue(input: BigDecimal): Boolean {
-            return input == input.toBigInteger().toBigDecimal()
+            return input == input.toInt().toBigDecimal()
         }
 
         /**
-         * Check whether a numeric value in <code>BigDecimal</code> type is in valid ranges of <code>BigInteger</code>.
+         * Check whether a numeric value in <code>BigDecimal</code> type is in valid ranges of <code>Long</code>.
          *
          * @param input numeric value in BigDecimal type
          * @return true if within valid ranges of BigInteger type, otherwise false
          */
+        @JvmStatic
         fun isDecimalLongValue(input: BigDecimal): Boolean {
-            return input == input.toBigInteger().toBigDecimal()
+            return input == input.toLong().toBigDecimal()
         }
     }
 }
