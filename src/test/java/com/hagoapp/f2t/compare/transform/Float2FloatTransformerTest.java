41487f82b7d882e51c866363aa718782001132e4
Chaojun Sun <suncjs@hagoapp.com>
2023-12-27 20:37:25 +0800

    draft

---
+++ src/test/java/com/hagoapp/f2t/compare/transform/Float2FloatTransformerTest.java
@@ -0,0 +1,21 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.f2t.compare.transform;
+
+import org.junit.jupiter.api.Test;
+
+class Float2FloatTransformerTest {
+
+    private final TransformCase[] cases = new TransformCase[]{
+
+    };
+
+    @Test
+    void testTransform() {
+        TransformCase.runCases(cases);
+    }
+}
