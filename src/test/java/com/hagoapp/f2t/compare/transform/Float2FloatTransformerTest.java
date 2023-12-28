465f274a29936e3dd5a90bfd77f74e62a2e8ede5
Chaojun Sun <suncjs@hagoapp.com>
2023-12-28 14:53:17 +0800

    complete Float2FloatTransformerTest

--- src/test/java/com/hagoapp/f2t/compare/transform/Float2FloatTransformerTest.java
+++ src/test/java/com/hagoapp/f2t/compare/transform/Float2FloatTransformerTest.java
@@ -6,16 +6,53 @@
 
 package com.hagoapp.f2t.compare.transform;
 
+import com.hagoapp.f2t.ColumnDefinition;
+import com.hagoapp.f2t.FileColumnDefinition;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.sql.JDBCType;
+import java.util.Set;
 
 class Float2FloatTransformerTest {
 
     private final TransformCase[] cases = new TransformCase[]{
-
+            new TransformCase(
+                    new FileColumnDefinition("", Set.of(), JDBCType.FLOAT),
+                    new ColumnDefinition("", JDBCType.FLOAT),
+                    1.1f, 1.1f, null, null
+            ),
+            new TransformCase(
+                    new FileColumnDefinition("", Set.of(), JDBCType.FLOAT),
+                    new ColumnDefinition("", JDBCType.DOUBLE),
+                    1.1f, (double) 1.1f, null, null
+            ),
+            new TransformCase(
+                    new FileColumnDefinition("", Set.of(), JDBCType.FLOAT),
+                    new ColumnDefinition("", JDBCType.DOUBLE),
+                    1.1f, (double) 1.1f, null, null
+            ),
+            new TransformCase(
+                    new FileColumnDefinition("", Set.of(), JDBCType.FLOAT),
+                    new ColumnDefinition("", JDBCType.DOUBLE),
+                    BigDecimal.valueOf(1.1d), BigDecimal.valueOf(1.1d).doubleValue(), null, null
+            ),
+            new TransformCase(
+                    new FileColumnDefinition("", Set.of(), JDBCType.DECIMAL),
+                    new ColumnDefinition("", JDBCType.DECIMAL),
+                    123f, BigDecimal.valueOf(123), null, null
+            ),
+            new TransformCase(
+                    new FileColumnDefinition("", Set.of(), JDBCType.FLOAT),
+                    new ColumnDefinition("", JDBCType.DOUBLE),
+                    null, null, null, null
+            )
     };
 
     @Test
     void testTransform() {
+        Assertions.assertTrue(cases.length > 0);
         TransformCase.runCases(cases);
     }
 }
